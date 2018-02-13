a44c0dec7632ce2202fb51b554b38a6551f7d594
Simon <simon@simonhamilton.ca>
2018-02-13 17:26:06 +0000

    use recursive function

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+
+
+    if (fromN === toN) {      // we start with the base case, when the 'loop' stops
+    return toN;
+  }
+
+  return fromN + sum(fromN + 1, toN);
 }
 
 module.exports = sum;
